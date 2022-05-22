7fda441e457bdb5fc810ae36f70036ff4074d512
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-05-22 10:24:11 +0100

    themed country page

--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 import { Country } from 'src/app/models/country.model';
+import { Theme } from 'src/app/models/theme.enum';
 import { CountryService } from 'src/app/services/country.service';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss']
 })
-export class CountryComponent implements OnInit {
+export class CountryComponent implements OnInit, OnDestroy {
 
   country$!: Observable<Country>;
+  theme!: Theme;
+  darkMode = false;
+  protected sub = new Subject();
   constructor(private route: ActivatedRoute,
               private router: Router,
               private countryService: CountryService,
-              private  title: Title) { }
+              private  title: Title,
+              private themeService: ThemeService) { }
 
   ngOnInit(): void {
+    this.themeService.currentTheme$.pipe(takeUntil(this.sub))
+    .subscribe(theme => {
+      this.theme = theme;
+      this.darkMode = this.theme === Theme.Dark;
+    });
+
     this.country$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
         const country = params.get('country')!;
@@ -27,6 +39,11 @@ export class CountryComponent implements OnInit {
         return this.countryService.getCountry(country);
       })
     );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.next();
+    this.sub.complete();
   }
 
   returnHome(): void {
