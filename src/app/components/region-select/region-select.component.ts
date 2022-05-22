80965bf81522fa8ad69ccdbb9552c9f745796a4c
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-05-22 09:58:15 +0100

    themed region selector

--- src/app/components/region-select/region-select.component.ts
+++ src/app/components/region-select/region-select.component.ts
@@ -1,16 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Theme } from 'src/app/models/theme.enum';
 
 @Component({
   selector: 'app-region-select',
   templateUrl: './region-select.component.html',
   styleUrls: ['./region-select.component.scss']
 })
-export class RegionSelectComponent {
+export class RegionSelectComponent implements OnChanges {
 
   showOptions = false;
   option = 'All';
+  darkMode = false;
   @Input() options: string[] = [];
+  @Input() theme!: Theme;
   @Output() optionChanged = new EventEmitter<string>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.darkMode = changes.theme.currentValue === Theme.Dark;
+  }
 
   toggleOptions(): void {
     this.showOptions = !this.showOptions;
